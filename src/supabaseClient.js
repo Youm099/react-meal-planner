dca61cbb9a349cc8884820f179856a2580af91e9
mba705236 <mba705236@gmail.com>
2026-05-20 13:01:13 +0000

    Moved Supabase keys to environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,8 @@
 
-''
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nkqqlcyncrtqrdshysbt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rcXFsY3luY3J0cXJkc2h5c2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMjQ2NTEsImV4cCI6MjA5NDcwMDY1MX0.SxfRVji7S0bKbm3QdXtge4F9UIS0jmOuhIbEUYmxg2A';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
